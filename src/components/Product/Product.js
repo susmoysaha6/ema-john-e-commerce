76ca255e569d4733b909823b732e64ca3a5f66e2
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-25 16:00:29 +0600

    Implement some style to product component

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -6,10 +6,15 @@ const Product = (props) => {
     return (
         <div className='product'>
             <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
-            <h3>{seller}</h3>
-            <h4>{price}</h4>
-            <p>{ratings}</p>
+            <div className='product-info'>
+                <p className='product-name'>{name}</p>
+                <p>Price: ${price}</p>
+                <p><small>Seller: {seller}</small></p>
+                <p><small>Ratings: {ratings} stars</small></p>
+            </div>
+            <button className='btn-cart'>
+                <p>Add to Cart</p>
+            </button>
         </div>
     );
 };
