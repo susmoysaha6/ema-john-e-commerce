7930a1957d3f8f0436a4e6b46c55edc74dce270e
susmoysaha6 <susmoysaha6@gmail.com>
2022-09-26 00:34:38 +0600

    Cart component created

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css'
 
@@ -25,8 +26,7 @@ const Shop = () => {
                 }
             </div>
             <div className="cart-container">
-                <h4>Order Summary</h4>
-                <p>Selected Items:{cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
