e43d4c8ab67261b12866dea682efe527429a15b2
susmoysaha6 <susmoysaha6@gmail.com>
2022-10-07 15:32:56 +0600

    children props added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = ({ cart }) => {
+const Cart = ({ cart, clearCart, children }) => {
     let total = 0;
     let shipping = 0;
     let quantity = 0;
@@ -20,6 +20,8 @@ const Cart = ({ cart }) => {
             <p>Total Shipping: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
+            <button onClick={clearCart}>Clear Cart</button>
+            {children}
         </div>
     );
 };
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import { removeFromDb } from '../../utilities/fakedb';
+import { Link, useLoaderData } from 'react-router-dom';
+import { deleteShoppingCart, removeFromDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
 
 const Orders = () => {
-    const { products, initialCart } = useLoaderData();
+    const { initialCart } = useLoaderData();
     const [cart, setCart] = useState(initialCart);
-
+    const clearCart = () => {
+        setCart([]);
+        deleteShoppingCart();
+    }
     const handleRemoveItem = (id) => {
         const remaining = cart.filter(product => product.id !== id)
         setCart(remaining);
@@ -18,10 +21,12 @@ const Orders = () => {
             <div className='orders-container'>
                 {
                     cart.map(product => <ReviewItem key={product.id} product={product} handleRemoveItem={handleRemoveItem}></ReviewItem>)
+                }{
+                    cart.length === 0 && <h2>No Items for Review. Please Shop More <Link to='/shop'>Shop</Link></h2>
                 }
             </div>
             <div className='cart-container'>
-                <Cart cart={cart}></Cart>
+                <Cart clearCart={clearCart} cart={cart}></Cart>
             </div>
 
         </div>
